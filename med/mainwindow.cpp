880e04beb4101006768e30abf9addb2b750e5ed6
Dmitrii Popov <popovdg@gmail.com>
2016-10-28 15:57:55 +0300

    Начало работы с БД

--- med/mainwindow.cpp
+++ med/mainwindow.cpp
@@ -2,6 +2,10 @@
 #include "ui_mainwindow.h"
 #include <QSplitter>
 #include <QTableView>
+#include <QSqlDatabase>
+#include <QDebug>
+#include <QSqlError>
+#include <QSqlTableModel>
 
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
@@ -14,6 +18,31 @@ MainWindow::MainWindow(QWidget *parent) :
     splitter->addWidget(ui->studiesBox);
     splitter->setChildrenCollapsible(false);
     ui->tableLayout->addWidget(splitter);
+
+    QSqlDatabase db = QSqlDatabase::addDatabase("QPSQL");
+    db.setPort(5433);
+    db.setDatabaseName("med");
+    bool ok = db.open();
+
+    QSqlTableModel *patientsModel = new QSqlTableModel(ui->patientsView, db);
+    patientsModel->setTable("patients");
+    patientsModel->setEditStrategy(QSqlTableModel::OnRowChange);
+    patientsModel->select();
+    patientsModel->setHeaderData(1, Qt::Horizontal, tr("ФИО"));
+    patientsModel->setHeaderData(2, Qt::Horizontal, tr("Дата рождения"));
+    patientsModel->setHeaderData(3, Qt::Horizontal, tr("Пол"));
+    patientsModel->setHeaderData(4, Qt::Horizontal, tr("Вес"));
+    ui->patientsView->setModel(patientsModel);
+    ui->patientsView->hideColumn(0);
+
+    QSqlTableModel *studiesModel = new QSqlTableModel(ui->studiesView, db);
+    studiesModel->setTable("studies");
+    studiesModel->setEditStrategy(QSqlTableModel::OnRowChange);
+    studiesModel->select();
+    studiesModel->setHeaderData(1, Qt::Horizontal, tr("Тип исследования"));
+    studiesModel->setHeaderData(2, Qt::Horizontal, tr("Дата исследования"));
+    ui->studiesView->setModel(studiesModel);
+    ui->studiesView->hideColumn(0);
 }
 
 MainWindow::~MainWindow()
