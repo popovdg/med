5ed92ab08d0d5ba675e8ef446a5b8232c0975bad
Dmitrii Popov <popovdg@gmail.com>
2016-10-28 11:51:13 +0300

    Скомпонован интерфейс пользователя

--- med/mainwindow.cpp
+++ med/mainwindow.cpp
@@ -1,11 +1,19 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
+#include <QSplitter>
+#include <QTableView>
 
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
     ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    QSplitter *splitter = new QSplitter(this);
+    splitter->setOrientation(Qt::Vertical);
+    splitter->addWidget(ui->patientsBox);
+    splitter->addWidget(ui->studiesBox);
+    splitter->setChildrenCollapsible(false);
+    ui->tableLayout->addWidget(splitter);
 }
 
 MainWindow::~MainWindow()
