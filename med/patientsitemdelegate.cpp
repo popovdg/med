33df1f2bdead70ebdf48a03416e99f6bf30b589c
popov <mac@imac.local>
2016-10-29 22:23:13 +0300

    Запрет на редактирование веса

--- med/patientsitemdelegate.cpp
+++ med/patientsitemdelegate.cpp
@@ -1,1 +1,7 @@
 #include "patientsitemdelegate.h"
+
+QWidget *PatientsItemDelegate::createEditor(QWidget *parent, const QStyleOptionViewItem &option, const QModelIndex &index) const
+{
+    if(index.column() == 5) return NULL;
+    return QStyledItemDelegate::createEditor(parent, option, index);
+}
--- med/patientsitemdelegate.h
+++ med/patientsitemdelegate.h
@@ -3,12 +3,18 @@
 
 #include <QStyledItemDelegate>
 
-
+/**  */
 class PatientsItemDelegate : public QStyledItemDelegate
 {
     Q_OBJECT
+
 public:
+
+    /** Конструктор */
     PatientsItemDelegate(QObject *parent = Q_NULLPTR) : QStyledItemDelegate(parent) {}
+
+    /** Создаёт редактор элемениов */
+    QWidget * createEditor(QWidget *, const QStyleOptionViewItem &, const QModelIndex &) const;
 };
 
 #endif // PATIENTSITEMDELEGATE_H
