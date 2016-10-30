cb531283aff5a378022b08c3ca59fe95e7a97671
popov <mac@imac.local>
2016-10-30 12:45:36 +0300

    Изменены инициализирующие данные БД

--- create_db.sh
+++ create_db.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 createdb `whoami`
-psql -d `whoami` -f db.sql
-psql -d `whoami` -f insert.sql
+psql -d `whoami` -f create_tables.sql
+psql -d `whoami` -f insert_data.sql
--- insert_data.sql
+++ insert_data.sql
@@ -1,7 +1,19 @@
-﻿insert into patients(fio, dob) values ('Иванов Иван Иванович', '1953-10-11');
-insert into patients(fio, dob) values ('Петрова Мария Ивановна', '1960-11-10');
-insert into patients(fio, dob) values ('Семёнов Семён Семёнович', '1970-01-15');
+﻿with patient as
+(
+	insert into patients(fio, dob) values ('Иванов Иван Иванович', '1953-10-11')
+	returning id
+),
+study1 as (insert into studies (patient, type, date) select id, 'a', '2016-04-05' from patient),
+study2 as (insert into studies (patient, type, date) select id, 'b', '2016-04-05' from patient)
+insert into studies (patient, type, date) select id, 'a', '2015-05-05' from patient;
 
-insert into studies (patient, type, date) values (1, 'a', '2016-04-05');
-insert into studies (patient, type, date) values (1, 'b', '2016-04-05');
-insert into studies (patient, type, date) values (2, 'a', '2016-04-05');
+
+with patient as
+(
+	insert into patients(fio, dob) values ('Иванова Мария Ивановна', '1960-11-11')
+	returning id
+),
+study as (insert into studies (patient, type, date) select id, 'a', '2016-10-05' from patient)
+insert into studies (patient, type, date) select id, 'c', '2014-04-05' from patient;
+
+insert into patients(fio, dob) values ('Сидоров Иван Петрович', '1953-10-11')
