a129b5ba653fbd43c99bb16d2e8e90e05c7441f0
popov <mac@imac.local>
2016-10-30 11:50:36 +0300

    Исправлен скрипт загружающий данные в БД

--- insert.sql
+++
@@ -1,7 +0,0 @@
-﻿insert into patients_view(id, fio, dob) values (1, 'Иванов Иван Иванович', '1953-10-11');
-insert into patients_view(id, fio, dob) values (2, 'Петрова Мария Ивановна', '1960-11-10');
-insert into patients_view(id, fio, dob) values (3, 'Семёнов Семён Семёнович', '1970-01-15');
-
-insert into studies (patient, type, date) values (1, 'a', '2016-04-05');
-insert into studies (patient, type, date) values (1, 'b', '2016-04-05');
-insert into studies (patient, type, date) values (2, 'a', '2016-04-05');
---
+++ insert_data.sql
@@ -0,0 +1,7 @@
+﻿insert into patients(fio, dob) values ('Иванов Иван Иванович', '1953-10-11');
+insert into patients(fio, dob) values ('Петрова Мария Ивановна', '1960-11-10');
+insert into patients(fio, dob) values ('Семёнов Семён Семёнович', '1970-01-15');
+
+insert into studies (patient, type, date) values (1, 'a', '2016-04-05');
+insert into studies (patient, type, date) values (1, 'b', '2016-04-05');
+insert into studies (patient, type, date) values (2, 'a', '2016-04-05');
